2904f0be83d9705ab109d899562e4edf739c24d2
anas idrissi <dsi.wetic@gmail.com>
2020-08-09 01:50:06 +0100

    initialize ses9

---
+++ pas important/index.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>acceuile</title>
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<header>
+<div class="logo">
+<a  href="www.ses9.com"><img src="tof/ses.png" title="www.ses9.com" alt="www.ses9.com" /></a><span><a><img src="tof/like.png" title="ses9 sur facebook" alt="facebook/ses9" /></a></span>
+<aside><a><img src="tof/twit.png" title="ses9 sur twiter" alt="twiter/ses9" /></a></aside>
+<k><a><img src="tof/g.png" title="ses9 sur google+" alt="google+/ses9" /></a></k>
+</div>
+<div class="text">
+play free games flash at ses9.com
+jouez des jeux gratuit dans ses9.com
+jeux gratuit
+free games
+flash games
+free flash games
+jeux flash gratuit
+</div>
+<center><div id="menu">
+<a href="index.html" title="www.ses9.com">acceuil</a>
+<a href="action/page/action.html" title="action">action</a>
+<a href="aventure/page/aventure.html" title="aventure">aventure</a>
+<a href="fille/page/fille.html" title="fille">fille</a>
+<a href="arcade/page/arcade.html" title="arcade">arcade</a>
+<a href="course/page/course.html" title="course">course</a>
+<a href="mario/page/mario.html" title="mario">mario</a>
+<a href="combat/page/combat.html" title="combat">combat</a>
+<a href="tire/page/tire.html" title="tire">tire</a>
+<a href="flappy/page/flappy.html" title="flappy">flappy</a>
+<a href="3d/page/3d.html" title="3D">3D</a>
+<a href="online/page/online.html" title="online">online</a>
+</div></center>
+</header>
+<nav class="didi">
+<section class="action">
+	<div class="tit">
+	<a href="action/page/action.html" title="action">action</a>  	
+	</div>
+    <center><div class="tof">
+    <a href="action/play/stunt.html"><img src="action/tof/stunt-master.gif" title="stunt master"/></a>
+    <a href="action/play/dwyt.htm"><img src="action/tof/dwyt.gif" title="dwyt"/></a>
+    <a href="action/play/mickey.htm"><img src="action/tof/mickey.jpg" title="mckey"/></a>
+    <a href="action/play/kooking.htm"><img src="action/tof/kooking.gif" title="kooking"/></a>
+    <a href="action/play/name.htm"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="action/play/name.htm"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="action/play/name.htm"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="action/play/name.htm"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="action/play/name.htm"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="action/play/name.htm"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="action/play/name.htm"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="action/play/name.htm"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="action/play/name.htm"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="action/play/name.htm"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="action/play/name.htm"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="action/play/name.htm"><img src="action/tof/stunt-master.gif"/></a>
+    </div></center>
+    </section>
+    <section class="adv">
+	<div class="tit">
+	<a href="aventure/page/aventure.html" title="adventure">adventure</a>  	
+	</div>
+    <center><div class="tof">
+    <a href="aventure/play/flappy.html"><img src="aventure/tof/flappy.jpg" title="flappy-birth"/></a>
+    <a href="aventure/play/.html"><img src="aventure/tof/mario.png" title="super-mario"/></a>
+    <a href="aventure/play/.html"><img src="aventure/tof/super-chick.jpg" title="super chick"/></a>
+    <a href="aventure/play/.html"><img src="aventure/tof/epic-ninja.gif" title="epic-ninja"/></a>
+    <a href="aventure/play/.html"><img src="aventure/tof/ray1.jpg" title="ray1"/></a>
+    <a href="aventure/play/.html"><img src="aventure/tof/siht-heads.gif" title="siht-heads"/></a>
+    <a href="aventure/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="aventure/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="aventure/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="aventure/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="aventure/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="aventure/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="aventure/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="aventure/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="aventure/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="aventure/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    </div></center>
+    </section>
+    <section class="fille">
+	<div class="tit">
+	<a href="fille/page/fille.html" title="fille">fille</a>  	
+	</div>
+    <center><div class="tof">
+    <a href="fille/play/.html" title="stylist-for-the-start"><img src="fille/tof/stylist.gif"/></a>
+    <a href="fille/play/.html" title="dinner-party-slacking"><img src="fille/tof/dinner.gif"/></a>
+    <a href="fille/play/.html" title="papa's-burgeria"><img src="fille/tof/papa.jpg"/></a>
+    <a href="fille/play/.html" title="valentine-surprise"><img src="fille/tof/valentine.jpg"/></a>
+    <a href="fille/play/.html" title="GAME-BEAUTY-RUSH"><img src="fille/tof/game.jpg"/></a>
+    <a href="fille/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="fille/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="fille/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="fille/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="fille/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="fille/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="fille/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="fille/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="fille/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="fille/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="fille/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    </div></center>
+    </section>
+        <section class="course">
+	<div class="tit">
+	<a href="course/page/course.html" title="course">course</a>  	
+	</div>
+    <center><div class="tof">
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="course/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    </div></center>
+    </section>
+      <section class="combat">
+	<div class="tit">
+	<a href="combat/page/combat.html" title="combat">combat</a>  	
+	</div>
+    <center><div class="tof">
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="combat/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    </div></center>
+    </section>
+        <section class="sport">
+	<div class="tit">
+	<a href="sport/page/sport.html" title="sport">sport</a>  	
+	</div>
+    <center><div class="tof">
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    <a href="sport/play/.html"><img src="action/tof/stunt-master.gif"/></a>
+    </div></center>
+    </section>
+
+</nav>
+<center><footer>
+<center><a href="index.html" title="www.ses9.com">acceuil</a>
+<a href="action/page/action.html" title="action">action</a>
+<a href="aventure/page/aventure.html" title="aventure">aventure</a>
+<a href="fille/page/fille.html" title="fille">fille</a>
+<a href="arcade/page/arcade.html" title="arcade">arcade</a>
+<a href="course/page/course.html" title="course">course</a>
+<a href="mario/page/mario.html" title="mario">mario</a>
+<a href="combat/page/combat.html" title="combat">combat</a>
+<a href="tire/page/tire.html" title="tire">tire</a>
+<a href="flappy/page/flappy.html" title="flappy">flappy</a>
+<a href="3d/page/3d.html" title="3D">3D</a>
+<a href="online/page/online.html" title="online">online</a>
+<a href="multiplayer/page/multiplayer.html" title="multiplayer">multiplayer</a>
+<a href="billard/page/billard.html" title="billard">billard</a>
+<a href="sport/page/sport.html" title="sport">sport</a>
+<a href="barbie/page/barbie.html" title="barbie">barbie</a>
+<a href="guerre/page/guerre.html" title="guerre">guerre</a>
+<a href="moto/page/moto.html" title="moto">moto</a>
+<a href="bebe/page/bebe.html" title="bébé">bébé</a>
+<a href="education/page/education.html" title="education">education</a>
+<a href="foot-ball/page/foot-ball.html" title="foot-ball">foot-ball</a>
+<a href="base-ball/page/base-ball.html" title="base-ball">base-ball</a>
+<a href="ninja/page/ninja.html" title="ninja">ninja</a>
+<a href="decoration/page/decoration.html" title="decoration">decoration</a>
+<a href="garcon/page/garcon.html" title="garçon">garçon</a>
+<a href="ferme/page/ferme.html" title="ferme">ferme</a>
+<a href="gateau/page/gateau.html" title="gateau">Gateau</a>
+<a href="magie/page/magie.html" title="magie">magie</a>
+<a href="parking/page/parking.html" title="parking">parking</a>
+<a href="puzzle/page/puzzle.html" title="puzzle">puzzle</a></center>
+<p><a href="#" tittle="arabic">arabic</a></p>
+<a href="mailto:ses9@gmail.com">contactez nous...</a>
+</footer></center>
+</body>
+</html>
